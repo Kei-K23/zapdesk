3df6df6a39e8c659816a8e49ea251d7099cd9074
Kei-K23 <keiksl2301@gmail.com>
2024-09-21 08:28:12 +0630

    Add basic setup for convex backend

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,6 @@
+import AuthScreen from "@/features/auth/components/auth-screen";
+import React from "react";
+
+export default function AuthPage() {
+  return <AuthScreen />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import AuthScreen from "@/features/auth/components/auth-screen";
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  return <AuthScreen />;
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    </main>
+  );
 }
