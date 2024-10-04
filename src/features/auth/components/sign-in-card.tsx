0d81687b7e64fc61eda2e00fa59de4435efca4be
Kei-K23 <keiksl2301@gmail.com>
2024-10-04 13:05:51 +0630

    Setup Passowrd auth with convex

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -10,18 +10,40 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FcGoogle } from "react-icons/fc";
 import { AuthFlow } from "../type";
-import { FaGithub } from "react-icons/fa";
+import { FaExclamationTriangle, FaGithub } from "react-icons/fa";
 import { Separator } from "@/components/ui/separator";
 import { useAuthActions } from "@convex-dev/auth/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 type SignInCardProps = {
   setAuthFlow: React.Dispatch<React.SetStateAction<AuthFlow>>;
 };
 
 export default function SignInCard({ setAuthFlow }: SignInCardProps) {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
   const [pending, setPending] = useState<boolean>(false);
   const { signIn } = useAuthActions();
+
+  const handlePasswordSignIn = (e: FormEvent) => {
+    e.preventDefault();
+    if (email === "") {
+      setError("Email field is required");
+      return;
+    }
+    if (password === "") {
+      setError("Password field is required");
+      return;
+    }
+
+    setPending(true);
+    signIn("password", { email, password, flow: "signIn" })
+      .catch(() => {
+        setError("Invalid email or password");
+      })
+      .finally(() => setPending(false));
+  };
 
   const handleProviderSignIn = (provider: "github" | "google") => {
     setPending(true);
@@ -37,31 +59,43 @@ export default function SignInCard({ setAuthFlow }: SignInCardProps) {
             Enter your email below to sign in to your account
           </CardDescription>
         </CardHeader>
+        {!!error && (
+          <div className="bg-destructive/15 rounded-md mx-6 px-3 py-2 text-destructive flex items-center gap-2 text-sm mb-4">
+            <FaExclamationTriangle />
+            {error}
+          </div>
+        )}
         <CardContent>
           <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                disabled={pending}
-                id="email"
-                type="email"
-                placeholder="mygmail@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                disabled={pending}
-                id="password"
-                type="password"
-                placeholder="password"
-                required
-              />
-            </div>
-            <Button disabled={pending} type="submit" className="w-full">
-              Sign In
-            </Button>
+            <form className="space-y-4" onSubmit={handlePasswordSignIn}>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  disabled={pending}
+                  id="email"
+                  type="email"
+                  placeholder="mygmail@example.com"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  disabled={pending}
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button disabled={pending} type="submit" className="w-full">
+                Sign In
+              </Button>
+            </form>
             <div className="flex items-center gap-3">
               <Separator className="flex-1" />
               <span>or</span>
