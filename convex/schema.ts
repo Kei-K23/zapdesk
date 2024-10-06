d46fa2a228c04cd9e39a2c74b824e0cee8de9be4
Kei-K23 <keiksl2301@gmail.com>
2024-10-06 11:54:51 +0630

    Define members schema in convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,6 +9,18 @@ const schema = defineSchema({
     userId: v.id("users"),
     joinCode: v.string(),
   }),
+  members: defineTable({
+    userId: v.id("users"),
+    workspaceId: v.id("workspaces"),
+    role: v.union(
+      v.literal("admin"),
+      v.literal("member"),
+      v.literal("moderator")
+    ),
+  })
+    .index("by_user_id", ["userId"])
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_workspace_id_user_id", ["workspaceId", "userId"]),
 });
 
 export default schema;
