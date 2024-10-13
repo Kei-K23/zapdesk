a722b4b7a0daa23f4db012686ed6c229a322831e
Kei-K23 <keiksl2301@gmail.com>
2024-10-13 09:55:38 +0630

    Add channel query

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as auth from "../auth.js";
+import type * as channels from "../channels.js";
 import type * as http from "../http.js";
 import type * as member from "../member.js";
 import type * as users from "../users.js";
@@ -31,6 +32,7 @@ import type * as workspaces from "../workspaces.js";
  */
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
+  channels: typeof channels;
   http: typeof http;
   member: typeof member;
   users: typeof users;
---
+++ convex/channels.ts
@@ -0,0 +1,32 @@
+import { getAuthUserId } from "@convex-dev/auth/server";
+import { query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getChannels = query({
+  args: {
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .unique();
+
+    if (!members) {
+      return [];
+    }
+
+    return await ctx.db
+      .query("channels")
+      .withIndex("by_workspace_id", (q) =>
+        q.eq("workspaceId", args.workspaceId)
+      )
+      .collect();
+  },
+});
