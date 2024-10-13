d274db480abffb8fa236f72be8008b8000f45d1a
Kei-K23 <keiksl2301@gmail.com>
2024-10-13 18:45:23 +0630

    Add create channel backend and client mutation functions

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,5 +1,5 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const getChannels = query({
@@ -30,5 +30,37 @@ export const getChannels = query({
       .collect();
 
     return channels;
+  },
+});
+
+export const createChannel = mutation({
+  args: {
+    name: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .unique();
+
+    if (!currentMember) {
+      throw new Error("Unauthorized");
+    }
+
+    if (currentMember.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    return await ctx.db.insert("channels", {
+      name: args.name.replace(/\s+/g, "-").toLocaleLowerCase(),
+      workspaceId: args.workspaceId,
+    });
   },
 });
