ad0893acbd7f61b8183cf6e8ff05316ab6024c95
Kei-K23 <keiksl2301@gmail.com>
2024-10-14 11:40:04 +0630

    Add create new member backend function

--- convex/member.ts
+++ convex/member.ts
@@ -1,5 +1,5 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { query, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { v } from "convex/values";
 import { Id, Doc } from "./_generated/dataModel";
 
@@ -24,6 +24,34 @@ export const currentMember = query({
         q.eq("workspaceId", args.workspaceId).eq("userId", userId)
       )
       .unique();
+  },
+});
+
+export const createNewMember = mutation({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const existingMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (existingMember) {
+      throw new Error("This user is already active member of this workspace");
+    }
+
+    return await ctx.db.insert("members", {
+      userId,
+      workspaceId: args.id,
+      role: "member",
+    });
   },
 });
 
