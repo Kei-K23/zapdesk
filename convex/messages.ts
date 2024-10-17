19cd2b98c05ae0d9547a1aff8291fa50ce6a5fa7
Kei-K23 <keiksl2301@gmail.com>
2024-10-17 09:49:31 +0630

    Add basic logic backend function to get messages for channel

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,7 +1,68 @@
 import { v } from "convex/values";
-import { mutation, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { Id } from "./_generated/dataModel";
+import { paginationOptsValidator } from "convex/server";
+
+const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
+  const messages = await ctx.db
+    .query("messages")
+    .withIndex("by_parent_message_id", (q) =>
+      q.eq("parentMessageId", messageId)
+    )
+    .collect();
+
+  if (messages.length === 0) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  // Get the latest thread message and member of that message
+  const lastMessage = messages[messages.length - 1];
+  const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+
+  if (!lastMessageMember) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
+
+  if (!lastMessageUser) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  return {
+    count: messages.length,
+    image: lastMessageUser.image,
+    timestamp: lastMessage._creationTime,
+  };
+};
+
+const populateReactions = async (ctx: QueryCtx, messageId: Id<"messages">) => {
+  return await ctx.db
+    .query("reactions")
+    .withIndex("by_message_id", (q) => q.eq("messageId", messageId))
+    .collect();
+};
+
+const populateUser = async (ctx: QueryCtx, userId: Id<"users">) => {
+  return await ctx.db.get(userId);
+};
+
+const populateMember = async (ctx: QueryCtx, memberId: Id<"members">) => {
+  return await ctx.db.get(memberId);
+};
 
 const getMember = async (
   ctx: QueryCtx,
@@ -56,5 +117,43 @@ export const createMessage = mutation({
       memberId: member._id,
       createdAt: Date.now(),
     });
+  },
+});
+
+export const getMessages = query({
+  args: {
+    channelId: v.optional(v.id("channels")),
+    parentMessageId: v.optional(v.id("messages")),
+    conversationId: v.optional(v.id("conversations")),
+    paginationOps: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    let conversationId = args.conversationId;
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+
+      conversationId = parentMessage.conversationId;
+    }
+
+    const results = await ctx.db
+      .query("messages")
+      .withIndex("by_channel_id_parent_message_id_conversation_id", (q) =>
+        q
+          .eq("channelId", args.channelId)
+          .eq("parentMessageId", args.parentMessageId)
+          .eq("conversationId", args.conversationId)
+      )
+      .order("desc")
+      .paginate(args.paginationOps);
+
+    return results;
   },
 });
--- convex/schema.ts
+++ convex/schema.ts
@@ -44,6 +44,7 @@ const schema = defineSchema({
     .index("by_member_id", ["memberId"])
     .index("by_channel_id", ["channelId"])
     .index("by_conversation_id", ["conversationId"])
+    .index("by_parent_message_id", ["parentMessageId"])
     .index("by_channel_id_parent_message_id_conversation_id", [
       "channelId",
       "parentMessageId",
