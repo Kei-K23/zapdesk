b2d6a04e287e072ca7c9ca5db01211dff2264c69
Kei-K23 <keiksl2301@gmail.com>
2024-11-03 11:43:17 +0630

    Add blog comment like client mutation and query functions

---
+++ src/features/blogs/mutation/use-create-blog-comment-like.tsx
@@ -0,0 +1,69 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useMemo, useState } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = {
+  blogId: Id<"blogs">;
+  commentId: Id<"comments">;
+};
+type ResponseType = Id<"blogCommentLikes"> | null;
+
+type Options = {
+  onSuccess?: (data: Id<"blogCommentLikes">) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export default function useCreateBlogCommentLike() {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<
+    "success" | "error" | "settled" | "pending" | null
+  >(null);
+
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
+  const mutation = useMutation(api.blogCommentLikes.createBlogCommentLike);
+
+  const mutate = useCallback(
+    async (values: RequestType, options?: Options) => {
+      try {
+        setData(null);
+        setError(null);
+        setStatus("pending");
+
+        const response = await mutation(values);
+        options?.onSuccess?.(response);
+        setStatus("success");
+      } catch (error) {
+        setError(error as Error);
+
+        options?.onError?.(error as Error);
+        if (options?.throwError) {
+          throw error;
+        }
+      } finally {
+        setStatus("settled");
+
+        options?.onSettled?.();
+      }
+    },
+    [mutation]
+  );
+
+  return {
+    data,
+    isPending,
+    isError,
+    isSuccess,
+    isSettled,
+    error,
+    status,
+    mutate,
+  };
+}
